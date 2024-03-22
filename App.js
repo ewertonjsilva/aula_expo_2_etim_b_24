b9a21d9481cf63dace7bbfcf9f7637ea144511c6
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-22 09:48:05 -0300

    aula 22-03

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'orange',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
+    padding: 18, 
   },
 });
