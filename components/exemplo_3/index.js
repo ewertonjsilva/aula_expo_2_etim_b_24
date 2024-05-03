395c078cf18166acf620a68fd2eef2f91bdb6ed3
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-05-03 09:47:11 -0300

    aula 03-05

--- components/exemplo_3/index.js
+++ components/exemplo_3/index.js
@@ -2,13 +2,20 @@ import { View, Text, Pressable } from 'react-native';
 
 import styles from './styles';
 
-export default function Exemplo_3 () {
+function Exemplo_3 () {
+
+    // declarar variáveis
+    // definir funções
+
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 3</Text> 
-            <Pressable>
+            
+            <Pressable onPress={() => alert('Oi')} style={styles.botao}>
                 <Text>Ex. botão</Text>
             </Pressable>
         </View>
     );
 }
+
+export default Exemplo_3;
